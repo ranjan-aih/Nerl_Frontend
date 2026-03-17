0af281499912e8f6fd90620e7cf4b2418f1de163
Ranjan Kumar Chauhan <ranjan@ai-horizon.io>
2026-03-17 18:04:37 +0530

    changes the api point

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://nerl.ai-horizon.io/api',
   timeout: 150000,
   withCredentials: true,
 });
